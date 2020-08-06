71220f45a826463f466e8ac687eaeb77eef3c90a
KimViktor522 <KimViktor522@users.noreply.github.com>
2020-08-06 09:20:00 +0700

    Update pari v stoke.py

--- pari v stoke.py
+++ pari v stoke.py
@@ -1,32 +1,24 @@
-for i in range(5):
-    print(i)
-    for j in range(5):
-        print(j, end = '')
-        if (j==2):
-            break
-    print()
-
-
-sum = 0
 def proces_str(str):
     for i in range(len(str)):
         check = 0
         for j in range(i+1,len(str)):
             if (str[i] == str[j]):
                 check = 1
-                str.replace(str[i],"")
-                str.replace(str[j],"")
+                str = str.replace(str[i], '0', 1)
+                str = str.replace(str[j], '0', 1)
                 break
-        print(str, str[i])
-        if (check == 1):
-            str.replace(str[i],"")
-            sum += 1
     return str
-#str = str(input())
-str = 'abbcddk'
+
+def search_min_sum(str):
+    sum = 0
+    for i in range(len(str)):
+        if (str[i] != '0'):
+            sum += 1
+    return sum
+
+str = str(input('Введите строку :'))
+#str = "abbcddka"
+print('Ваша строка: ', str)
 str = proces_str(str)
-if (str == ''):
-    print('Мин нужно удалить :',sum)
-else:
-    print('Ошибка!!!')
+print('Мин нужно удалить :',search_min_sum(str))
 
