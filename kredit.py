69df0fac1b8181847e935388e1dd5dc010351dec
KimViktor522 <KimViktor522@users.noreply.github.com>
2021-05-31 17:18:33 +0700

    1

--- kredit.py
+++ kredit.py
@@ -1,5 +1,5 @@
 import math
 n = list(map(float, input().split()))
-M = (n[0]*(n[1]/100)*pow(1+(n[1]/100),n[2]))/(12*(pow((1+(n[1]/100)),n[2])-1))
-S = (M*12)*n[2]
+M = (n[0]*(n[2]/100)*pow(1+(n[2]/100),n[1]))/(12*(pow((1+(n[2]/100)),n[1])-1))
+S = (M*12)*n[1]
 print(round(M, 2),round(S, 2))
