92351a3435804e086205440ff04e4fa7e70b3fc2
KimViktor522 <KimViktor522@users.noreply.github.com>
2021-05-31 16:59:53 +0700

    fix again)

--- teugol.py
+++ teugol.py
@@ -1,11 +1,11 @@
 a = int(input())
 b = int(input())
 c = int(input())
-if (a >= 100 or b >= 100 or c >= 100 or a + b > c or b + c > a or c + a > b):
+if (a >= 100 or b >= 100 or c >= 100 or a + b < c or b + c < a or c + a < b):
     print("NO")
 elif (b == c and a == c):
-        print("RVS")
+    print("RVS")
 elif (b == c or a == c or a == c):
-        print("RVB")
+    print("RVB")
 else:
     print("RZS")
